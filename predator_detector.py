8569c1598f2cfbb7453b6ad461682c4fe77ac4a1
Josh Bergman <incoming@joshbergman.io>
2024-04-01 20:28:38 -0400

    updares

--- predator_detector.py
+++ predator_detector.py
@@ -1,7 +1,15 @@
-from gpiozero import MotionSensor
+from gpiozero import MotionSensor, LED
 
 pir = MotionSensor(4)
+red = LED(17)
+# yellow = LED(21)
+# green = LED(20)
+
+# red.blink()
+# yellow.blink()
+# green.blink()
 
 while True:
 	pir.wait_for_motion()
 	print("Motion detected!!")
+	red.blink()
---
+++ sound2.py
@@ -0,0 +1,6 @@
+from pygame import mixer
+import pygame
+pygame.init()
+mixer.init()
+mixer.music.load('mixkit-flock-of-wild-geese-20.wav')
+pygame.mixer.music.play()
